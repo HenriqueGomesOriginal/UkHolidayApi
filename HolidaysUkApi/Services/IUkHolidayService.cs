8a5a65837692dc8b18774126269501c7535fcba0
henriquegomesoriginal <henriquegomesoriginal@outlook.com>
2022-06-06 22:15:12 -0300

    Connection and deploy to azure

--- HolidaysUkApi/Services/IUkHolidayService.cs
+++ HolidaysUkApi/Services/IUkHolidayService.cs
@@ -5,5 +5,6 @@ namespace HolidaysUkApi.Services
     public interface IUkHolidayService
     {
         public Task<ItemReturn> GetHolidayUk(FilterParams filterParams);
+        public Task<ItemReturn> GetAllHolidayUk();
     }
 }
--- HolidaysUkApi/Services/Implementation/UkHolidayService.cs
+++ HolidaysUkApi/Services/Implementation/UkHolidayService.cs
@@ -1,5 +1,6 @@
 ﻿using HolidaysUkApi.Configuration;
 using HolidaysUkApi.Models;
+using HolidaysUkApi.Models.DataReturn;
 using Microsoft.Extensions.Options;
 
 namespace HolidaysUkApi.Services.Implementation
@@ -13,7 +14,85 @@ namespace HolidaysUkApi.Services.Implementation
             _azureFunctions = azureFunctions.Value ?? throw new ArgumentNullException(nameof(_azureFunctions));
         }
 
+        // Return object in range of date
+        public List<Events> FilterItemDate(FilterParams filterParams, List<Events> data)
+        {
+            var events = new List<Events>();
+
+            foreach (var item in data)
+            {
+                // Convert string to datetime
+                var date = DateTime.Parse(item.date);
+                if (date.Year == filterParams.date.Year)
+                {
+                    events.Add(item);
+                }
+            }
+
+            return events;
+        }
+
+        // Find location from Data
+        public ItemReturn FilterItem(FilterParams filterParams, ItemReturn item)
+        {
+            // Init vars
+            var ret = new ItemReturn();
+            var events = new List<Events>();
+
+            // Compare location and call filter for dates
+            switch (filterParams.location)
+            {
+                case "northern-ireland":
+                    events = FilterItemDate(filterParams, item.northIreland.events);
+                    return new ItemReturn
+                    {
+                        northIreland = new DataReturn
+                        {
+                            division = item.northIreland.division,
+                            events = events
+                        },
+                        england = new DataReturn(),
+                        scotland = new DataReturn()
+                    };
+                case "england-and-wales":
+                    events = FilterItemDate(filterParams, item.england.events);
+                    return new ItemReturn
+                    {
+                        england = new DataReturn
+                        {
+                            division = item.england.division,
+                            events = events
+                        },
+                        northIreland = new DataReturn(),
+                        scotland = new DataReturn()
+                    };
+                case "scotland":
+                    events = FilterItemDate(filterParams, item.scotland.events);
+                    return new ItemReturn
+                    {
+                        scotland = new DataReturn
+                        {
+                            division = item.scotland.division,
+                            events = events
+                        },
+                        england = new DataReturn(),
+                        northIreland = new DataReturn()
+                    }; ;
+                default:
+                    return null;
+            };
+        }
+
         public async Task<ItemReturn> GetHolidayUk(FilterParams filterParams)
+        {
+            // Get all JSON result
+            var item = await GetAllHolidayUk();
+            return FilterItem(filterParams, item);
+        }
+
+
+        // Download JSON file from Azure Function
+        public async Task<ItemReturn> GetAllHolidayUk()
         {
             try
             {
